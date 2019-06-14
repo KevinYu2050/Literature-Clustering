59c92b930a1412d38b6e480b8f4934751af9b45a
Valjean <valjean1832@outlook.com>
2019-06-14 11:18:31 +0800

    Reorganized data for tensorflow training

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,6 +1,8 @@
 import os
 import re
-
+import random
+import pandas as pd 
+import numpy as np
 
 def read_files(dir):
     content = []
@@ -20,22 +22,47 @@ def preprocessing(booklist):
     start_pattern = '[***] START OF THIS PROJECT GUTENBERG EBOOK'
     end_pattern = 'End of Project Gutenberg'
 
-    booklist =[list(filter(None, [sent.replace('\n', '') for sent in book])) for book in booklist]
-    booklist =[[sent.replace('\xa0', '') for sent in book] for book in booklist]
+    booklist = [list(filter(None, [sent.replace('\n', '') for sent in book])) for book in booklist]
+    booklist = [[sent.replace('\xa0', '') for sent in book] for book in booklist]
+    booklist = [[sent for sent in book if not re.match('^[^a-z]*$', sent)] for book in booklist]
 
     for book in booklist:
         for sent in book:
             if re.match(start_pattern, sent):
                 index = book.index(sent)
                 del book[:index+1]
-            if re.match(end_pattern, sent):
+            elif re.match(end_pattern, sent):
                 index = book.index(sent)
                 del book[index:]
-            if sent == '':
-                del sent
     
 
     return booklist 
 
 
-print(preprocessing(read_files('./gutenberg_shakespeare/')))
+def create_dataset(booklist, label):
+    # Divide the dataset into basic units of (sent_data, label) tuples
+    new_list = []
+    for book in booklist:
+        from_ = int(len(book)/10)
+        to = int(9 * len(book)/10)
+        for sent in book[from_:to]:
+            new_list.append((sent, label))
+
+    return new_list
+
+
+# Save the processed data into new txt files to fit in the tensorflow.data pipeline
+skp = create_dataset(preprocessing(read_files('./gutenberg_shakespeare/')), 'Shakespearean')
+non_skp = create_dataset(preprocessing(read_files('./gutenberg/')), 'Non-Shakespearean')
+
+dataset = skp + non_skp[:len(skp)]
+random.shuffle(dataset)
+with open('./data.txt', 'w+', encoding="utf-8") as f:
+    for i in dataset:
+        f.write(i[0])
+        f.write('\n')
+
+with open('./label.txt', 'w+', encoding="utf-8") as f:
+    for i in dataset:
+        f.write(i[1])
+        f.write('\n')
